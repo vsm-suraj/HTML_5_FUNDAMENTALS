51212aa37347f484d6ca29188c297e9eb841aeea
SURAJ <suraj@getvsm.com>
2018-04-09 14:03:16 +0530

    storing user input in local storage

---
+++ lecture26/external.js
@@ -0,0 +1,15 @@
+if(Modernizr.localstorage){
+    var comment = document.getElementById('comment'),
+
+    updateInterval;
+
+    comment.addEventListener('focus',function(){
+        updateInterval = setInterval(function(){
+            localStorage.comment = comment.value;
+        },2000)
+    },false)
+    
+    comment.addEventListener('blur',function(){
+        
+    })
+}
